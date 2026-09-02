65198e295321fd7566c03e7bac6e32eee7a40729
Ximena  Zamora Rodriguez <sofia@zamoras.me>
2026-09-02 23:05:38 +0000

    definetely running and getting the ball rolling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
+    int file_size = 100;
+    double sales = 9.99;
+    
     std::cout << "Hello World"; //This line is called a statement
+    
+    std::cout << file_size;
+
     return 0; //0 tells operating system that the program has ended successfully
 }
+
