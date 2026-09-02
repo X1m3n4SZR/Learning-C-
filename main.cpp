344fa5e13afc86abd47ff37c6c221cf11b883bfa
X1m3n4SZR <sofia@zamoras.me>
2026-09-02 14:01:12 -0700

    getting started

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World"; //This line is called a statement
+    return 0; //0 tells operating system that the program has ended successfully
+}
