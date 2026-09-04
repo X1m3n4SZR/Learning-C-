6f1ec729a365d35831cfe113174c6f623cf6194d
Ximena  Zamora Rodriguez <sofia@zamoras.me>
2026-09-04 19:14:37 +0000

    continuing on with exercises and making different files so that all the code isn't in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
 int main() {
-    int file_size = 100;
+    //int file_size = 0; //C++ doesn't automatically initalize variable to 0
+    //int counter = 0;
+
+    //exercise 1, switching values of a and b
+    int a = 1;
+    int b = 2;
+    int temp = a;
+    a = b;
+    b = temp;
+    std::cout << a;
+
+    //constants
+    const double pi = 3.14; // use const to declare something as a constant
+
+    //naming conventions
+    int file_size; // Snake Case, use lowercase letters for var and const and use _ to separate words
+    int FileSize; // Pascal Case, use uppercase and combines words
+    int fileSize; // Camel Case, first word lower second word upper
+    int iFileSize; // Hungarian Notation, very old and not really relevant but fun to know
+
     double sales = 9.99;
     
     std::cout << "Hello World"; //This line is called a statement
---
+++ pt2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // mathematical expressions
+    int x = 10;
+    x = x + 1;
+    x++; //increment operator, x-- decrement operator 
+    //int z = x + y; // for division, must convert one of the numbers from int to double
+                   // % returns remainder
+    std::cout << x;
+
+    return 0;
+}
