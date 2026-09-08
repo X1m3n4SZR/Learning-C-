21d01b73c102bcf899549b6474bb4510ada49f68
Ximena  Zamora Rodriguez <sofia@zamoras.me>
2026-09-08 03:17:39 +0000

    finished with math sections

--- math.cpp
+++ math.cpp
@@ -2,14 +2,27 @@
 
 int main()
 {
-    // mathematical expressions
-    int x = 10;
+    // Mathematical Expressions
+    //int x = 10;
     //x = x + 1;
-    x++; //increment operator, x-- decrement operator 
-    int z = ++x;
+    //x++; //increment operator, x-- decrement operator 
+    //int z = ++x;
     //int z = x + y; // for division, must convert one of the numbers from int to double
                    // % returns remainder
-    std::cout << x;
+    //std::cout << x;
+    //std:: cout << z;
+
+    // change order using ()
+    // * and / have higher priority
+    // than + and -
+    //double y = (1 + 2) * 3;
+    //std:: cout << y;
+
+    //exercise
+    double r = 10;
+    double s = 5;
+    double z = (r + 10)/ (3 * s);
+
     std:: cout << z;
 
     return 0;
