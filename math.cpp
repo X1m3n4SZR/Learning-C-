fed19b8d0c8e211dab46172fa54edec324fa375a
Ximena  Zamora Rodriguez <sofia@zamoras.me>
2026-09-08 03:09:30 +0000

    learning about mth expressions and operations

--- math.cpp
+++ math.cpp
@@ -4,11 +4,13 @@ int main()
 {
     // mathematical expressions
     int x = 10;
-    x = x + 1;
+    //x = x + 1;
     x++; //increment operator, x-- decrement operator 
+    int z = ++x;
     //int z = x + y; // for division, must convert one of the numbers from int to double
                    // % returns remainder
     std::cout << x;
+    std:: cout << z;
 
     return 0;
 }
